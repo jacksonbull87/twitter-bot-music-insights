ec577c5dfec8ed97f0dc3c540b91fbb004507929
Jackson <jacksonbull1987@gmail.com>
2020-10-22 13:37:16 -0400

    python script to generate artist with biggest change in spotify popularity over 7 days, then post to twitter account

---
+++ best_emerging_artist_tweet.py
@@ -0,0 +1,39 @@
+import tweepy
+from cm_config import  config
+from cm_api import *
+from helper_funct import *
+from twitter_bot import *
+
+
+
+
+
+#################################################
+#generate api token for chartmetric api access
+rt = config['refresh_token']
+api_token = get_api_token(rt)
+################################################
+
+current_date = generate_today_date()
+
+before_date = generate_one_week_prior_date()
+
+#collect top200 chart data for current date
+data = get_spotify_charts(api_token, current_date, 'US', 'regional', 'daily')
+
+#parse data and get title, artist, artists ids, and current spotify popularity 
+parsed_data = parse_top200_popularity(data)
+
+df2 = add_popularity_before_after(before_date, current_date, parsed_data)
+
+title, artist,artist_id, before_pop, after_pop, pop_change = get_most_successful_artist(df2)
+hashartist = artist.replace(" ", "",)
+
+#get spotify url for artist
+spot_url = get_spotify_url(api_token, artist_id)
+
+#instantiatiate twitter bot object
+bot = instantiate_twitter_bot()
+
+bot.update_status("This week the artist known as #{} gained the biggest increase in @spotify popularity with their song '{}'\nTheir popularity increased by {}% in just 7 days\n#dataanalysis #musicdiscovery powered by @Chartmetric code: https://bit.ly/3dN4UvG\n{}".format(hashartist, title, round(pop_change/before_pop *100, 2), spot_url))
+
