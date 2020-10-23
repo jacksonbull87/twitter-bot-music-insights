e1f4b83dd045e1649d53df148f59d0159d5019e2
Jackson <jacksonbull1987@gmail.com>
2020-10-23 14:44:28 -0400

    changed filename for monday and tuesday scripts

---
+++ viral_shazam.py
@@ -0,0 +1,27 @@
+import tweepy
+from cm_config import  config
+from cm_api import *
+from helper_funct import *
+from twitter_bot import *
+
+
+#################################################
+
+rt = config['refresh_token']
+api_token = get_api_token(rt)
+################################################
+
+#generate today's date to make api call with
+date = generate_today_date()
+
+#collects title, artist, velocity, and artist id for most viral shazam track
+title, artist, velocity, artist_id = get_shazam_most_viral_track(api_token,date, country_code='US')
+hashartist = artist.replace(" ", "",)
+
+#get spotify url for artist
+spot_url = get_spotify_url(api_token, artist_id)
+
+#instantiatiate twitter bot object
+bot = instantiate_twitter_bot()
+
+bot.update_status("'{}' by #{} is catching alot of people's attention this past week on #shazam\nSo much so that its average change in rank over 7-days is {}\n#dataanalysis #velocity #viral Powered by @Chartmetric\n{}".format(title, hashartist, round(velocity, 2), spot_url))
---
+++ viral_tiktok.py
@@ -0,0 +1,34 @@
+#this program gerates a post that contains the song with the highest velocity for the past 7-days on tiktok...most viral track
+
+import tweepy
+from cm_config import  config
+from cm_api import *
+from helper_funct import *
+from twitter_bot import *
+
+
+#################################################
+
+rt = config['refresh_token']
+api_token = get_api_token(rt)
+################################################
+
+#generate today's date to make api call with
+date = generate_today_date()
+
+#collects data from today's top 100 tracks on tiktok for the week
+data = get_tiktok_chart_data(api_token, 'tracks', date, 'weekly')
+
+title, artist, velocity, cm_id = parse_viral(data)
+hashartist = artist.replace(" ", "",)
+
+#get chartmetric artist id
+artist_id = get_track_metadata(api_token, cm_id)['artists'][0]['id']
+
+#get spotify url for artist
+spot_url = get_spotify_url(api_token, artist_id)
+
+#instantiatiate twitter bot object
+bot = twitter_bot()
+
+bot.update_status("'{}' by #{} has a velocity metric of {},\nmaking it the most viral song this week on #tiktok #dataanalytics Powered by @Chartmetric\n{}".format(title, hashartist, round(velocity, 2), spot_url))
