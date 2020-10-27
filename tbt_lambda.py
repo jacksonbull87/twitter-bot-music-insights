0630654db5f8a139b527800b7cc21c74ff1a6a43
Jackson <jacksonbull1987@gmail.com>
2020-10-27 13:06:42 -0400

    lambda function for AWS service

---
+++ tbt_lambda.py
@@ -0,0 +1,51 @@
+
+
+
+import json
+import logging
+from cm_config import  config
+from cm_api import *
+from helper_funct import *
+from twitter_bot import *
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def tweet(event, context):
+    #################################################
+    #generate api token for chartmetric api access
+    rt = config['refresh_token']
+    api_token = get_api_token(rt)
+    ################################################
+
+    #generates the date one year prior to make api call
+    date = generate_date_one_year_ago()
+
+    #extracks data for SPotify Top 200 chart for date generated above
+    data = get_spotify_charts(api_token, date, 'US', 'regional', 'daily')
+
+    #parses data and save title and artist as two variables
+    title, artist = parse_data(data)
+    hashartist = artist.replace(" ", "",)
+
+    #instantiatiate twitter bot object
+    bot = instantiate_twitter_bot()
+
+    message = "One Year Ago on {}: \nThe #1 song was '{}' by #{}\n@spotify #top200 #throwbackthursday Powered by @Chartmetric".format(date, title, hashartist)
+
+    bot.update_status(message)
+
+    body = {
+        "message": message,
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    logger.info(message)
+
+    return response
